bd4a52ecf708a4e31a67b8abe1ae1f16a05875b8
DarkThomaslol <92427564+DarkThomaslol@users.noreply.github.com>
2022-11-10 21:05:02 +0100

    1.8

--- js/project.js
+++ js/project.js
@@ -2,3 +2,18 @@ function start(){
     document.getElementById("sidebarProject").style.backgroundColor = "#50527d";
     indexPage = false;
 }
+
+function searchStuff(){
+    let input = document.getElementById('searchBar').value;
+    input = input.toLowerCase();
+    let listItems = document.getElementsByClassName('searchItems');
+
+    for (let i = 0; i < listItems.length; i++) {
+        if (!listItems[i].innerHTML.toLowerCase().includes(input)) {
+            listItems[i].style.display = "none";
+        }
+        else {
+            listItems[i].style.display = "list-item";
+        }
+    }
+}
