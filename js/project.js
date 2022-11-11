78a724d13bb7381b975764af7565c871deb37e4d
DarkThomaslol <92427564+DarkThomaslol@users.noreply.github.com>
2022-11-11 20:47:06 +0100

    1.9

--- js/project.js
+++ js/project.js
@@ -1,6 +1,8 @@
 function start(){
     document.getElementById("sidebarProject").style.backgroundColor = "#50527d";
     indexPage = false;
+    const project = projectF();
+    recent(project.title, project.text);
 }
 
 function searchStuff(){
@@ -17,3 +19,56 @@ function searchStuff(){
         }
     }
 }
+
+function recent(title, text){
+    document.getElementById("titleRecent").innerHTML = title;
+    document.getElementById("textRecent").innerHTML = text;
+}
+
+function change(title, text){
+    document.getElementById("titleProject").innerHTML = title;
+    document.getElementById("textProject").innerHTML = text;
+}
+
+function projectF(){
+    const title = document.getElementById("projectF").innerHTML;
+    const text = "text";
+    change(title, text);
+    return {title, text};
+}
+
+function projectE(){
+    const title = document.getElementById("projectE").innerHTML;
+    const text = "text le le la";
+    change(title, text);
+    return {title, text};
+}
+
+function projectD(){
+    const title = document.getElementById("projectD").innerHTML;
+    const text = "text le le la";
+    change(title, text);
+    return {title, text};
+}
+
+function projectC(){
+    const title = document.getElementById("projectC").innerHTML;
+    const text = "text le le la";
+    change(title, text);
+    return {title, text};
+}
+
+function projectB(){
+    const title = document.getElementById("projectB").innerHTML;
+    const text = "text le le la";
+    change(title, text);
+    return {title, text};
+}
+
+function projectA(){
+    const title = document.getElementById("projectA").innerHTML;
+    const text = "text le le la";
+    change(title, text);
+    return {title, text};
+}
+
