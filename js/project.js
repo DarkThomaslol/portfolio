285079b6d18d0f90a4428b64ab4a0828bf4a4fe8
DarkThomaslol <92427564+DarkThomaslol@users.noreply.github.com>
2022-11-23 16:49:14 +0100

    b

--- js/project.js
+++ js/project.js
@@ -22,7 +22,7 @@ function searchStuff(){
 }
 
 function randomProject(){
-    let number = Math.floor(Math.random() * 6);
+    let number = Math.floor(Math.random() * 7);
     switch (number){
         case 0:
             projectA();
@@ -42,18 +42,24 @@ function randomProject(){
         case 5:
             projectF();
             break;
+        case 6:
+            projectG();
+            break;
         default:
             break;
     }
 }
 
 function recent(title, text, img){
+    document.getElementById("titleRecent").style.fontSize = "40px";
+    document.getElementById("textRecent").style.fontSize = "30px";
     document.getElementById("titleRecent").innerHTML = title;
     document.getElementById("textRecent").innerHTML = text;
     document.getElementById("imgRecent").src = img;
 }
 
 function change(title, text, img){
+    document.getElementById("imgProject").style.width = "40%";
     document.getElementById("titleProject").innerHTML = title;
     document.getElementById("textProject").innerHTML = text;
     document.getElementById("imgProject").src = img;
@@ -61,7 +67,7 @@ function change(title, text, img){
 
 function projectG(){
     const title = document.getElementById("projectG").innerHTML;
-    const text = "text";
+    const text = "";
     const img = "../images/template/Bongo-Cat-png-3.png";
     change(title, text, img);
     return {title, text, img};
@@ -69,7 +75,7 @@ function projectG(){
 
 function projectF(){
     const title = document.getElementById("projectF").innerHTML;
-    const text = "text";
+    const text = "";
     const img = "";
     change(title, text, img);
     return {title, text, img};
@@ -77,7 +83,7 @@ function projectF(){
 
 function projectE(){
     const title = document.getElementById("projectE").innerHTML;
-    const text = "text le le la";
+    const text = "";
     const img = "";
     change(title, text, img);
     return {title, text, img};
@@ -85,7 +91,7 @@ function projectE(){
 
 function projectD(){
     const title = document.getElementById("projectD").innerHTML;
-    const text = "text le le la";
+    const text = "";
     const img = "";
     change(title, text, img);
     return {title, text, img};
@@ -93,7 +99,7 @@ function projectD(){
 
 function projectC(){
     const title = document.getElementById("projectC").innerHTML;
-    const text = "text le le la";
+    const text = "";
     const img = "";
     change(title, text, img);
     return {title, text, img};
@@ -101,16 +107,23 @@ function projectC(){
 
 function projectB(){
     const title = document.getElementById("projectB").innerHTML;
-    const text = "text le le la";
-    const img = "";
+    const text = "";
+    const img = "../images/projects/appformat/appformat.png";
     change(title, text, img);
     return {title, text, img};
 }
 
 function projectA(){
     const title = document.getElementById("projectA").innerHTML;
-    const text = "text le le la";
-    const img = "";
+    const text = "When I started this project, " +
+        "somewhere in the first year of my education (Software development) in my spare time, " +
+        "I thought it would be fun to learn some javascript. " +
+        "So my idea was to make a game that was playable on the browser, " +
+        "but I wanted my game to look nice with art. " +
+        "So of course I tried to do that with pixelart. " +
+        "It worked fine and everything fitted but it took way to long to do because I'm not a great artist. " +
+        "After a while I stopped having faith in it and stopped working on it.";
+    const img = "../images/projects/javaWebgame/pixelMap.png";
     change(title, text, img);
     return {title, text, img};
 }
